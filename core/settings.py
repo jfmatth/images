e2771a67472954e6b3cc047ac556e174ac81b909
John Matthew <john@compunique.com>
2024-09-16 19:11:36 -0700

    add logging

--- core/settings.py
+++ core/settings.py
@@ -132,6 +132,21 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 import os
 import dj_database_url
 
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "INFO",
+    },
+}
+
+
 ALLOWED_HOSTS = ["*"]
 DEBUG = False or 'DEBUG' in os.environ
 
